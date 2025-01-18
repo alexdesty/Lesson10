6e899d3426feb172fec090c36b2f39607b71721e
alexdesty <alex.desty@gmail.com>
2025-01-18 20:57:31 +0300

    Add code

---
+++ Lesson10/Exceptions/WrongUserInputException.cs
@@ -0,0 +1,12 @@
+﻿namespace Lesson10.Exceptions;
+public class WrongUserInputException : Exception
+{
+    public WrongUserInputException()
+    {
+    }
+
+    public WrongUserInputException(string message) : base(message)
+    {
+    }
+}
+
---
+++ Lesson10/Models/Person.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+using Lesson10.Exceptions;
+
+namespace Lesson10.Models;
+
+internal class Person
+{
+    private string _name;
+
+    private string _surname;
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            InputCheck(value);
+            _name = value;
+        }
+    }
+
+    public string Surname
+    {
+        get
+        {
+            return _surname;
+        }
+        set
+        {
+            InputCheck(value);
+            _surname = value;
+        }
+    }
+
+    public string InputCheck(string inputText)
+    {
+        if (String.IsNullOrEmpty(inputText))
+        {
+            throw new EmptyStringException("Nothing entered");
+        }
+        Regex regex = new Regex(@"^[a-zA-Z-]+$");
+        Match match = regex.Match(inputText);
+        if (!match.Success)
+        {
+            throw new EmptyStringException("Invalid character used");
+        }
+        return inputText;
+    }
+}
+
